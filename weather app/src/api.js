7970ce4037ef649254f4fa19f05f9dc9c4fbee20
architchopra <57140059+architchopra@users.noreply.github.com>
2021-07-05 21:22:27 +0000

    Archit Changes

--- weather app/src/api.js
+++ weather app/src/api.js
@@ -30,7 +30,7 @@ function initMap(position) {
   console.log(position);
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: position.coords.latitude ,lng: position.coords.longitude },
-    zoom: 8,
+    zoom: 8
   });
 }
 
